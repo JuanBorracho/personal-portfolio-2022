b1e4954e6ce9271e55f49234280330ad460dfae9
John Stanley <john.stanley90@gmail.com>
2022-01-24 10:43:51 +0000

    finished setting up tasks in gulpfile

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,14 +1,1 @@
-const mobileIconContainer = document.getElementById('hamburger');
-const mobileIcon = document.getElementById('mobile-icon');
-const mobileMenu = document.getElementById('mobile-menu');
-const exitIcon = document.getElementById('exit-icon');
-
-mobileIconContainer.addEventListener('click', () => {
-  mobileMenu.classList.toggle('open');
-  mobileIcon.classList.toggle('none');
-});
-
-exitIcon.addEventListener('click', () => {
-  mobileMenu.classList.toggle('open');
-  mobileIcon.classList.toggle('none');
-});
+const mobileIconContainer=document.getElementById("hamburger"),mobileIcon=document.getElementById("mobile-icon"),mobileMenu=document.getElementById("mobile-menu"),exitIcon=document.getElementById("exit-icon");mobileIconContainer.addEventListener("click",()=>{mobileMenu.classList.toggle("open"),mobileIcon.classList.toggle("none")}),exitIcon.addEventListener("click",()=>{mobileMenu.classList.toggle("open"),mobileIcon.classList.toggle("none")});
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,40 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
-// Sass Task - compile to css file, minify, sourcemaps
+// Styles Task - compile scss to css, minify, sourcemaps, autoprefixer
 function styles() {
   return gulp
     .src('src/sass/**/*.scss', { sourcemaps: true })
     .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('dist', { sourcemaps: '.' }))
     .pipe(browserSync.stream());
 }
 
-// Copy HTML files to dist folder
+// HTML task - copy html files from src to dist
 function html() {
   return gulp.src('src/*.html').pipe(gulp.dest('dist'));
 }
 
-//Copy images to dist folder
+// Image task - copy images from src to dist
 function images() {
   return gulp.src('src/img/*').pipe(gulp.dest('dist/img'));
 }
 
-// JavaScript Task
+// JavaScript Task - move src folder to dist and minify
 function js() {
-  return gulp.src('src/scripts/*').pipe(gulp.dest('dist/scripts'));
+  return gulp
+    .src('src/scripts/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
 }
 
-// Watch function
+// Watch task
 function watch() {
   browserSync.init({
     server: {
@@ -35,9 +43,10 @@ function watch() {
   });
   gulp.watch('src/*.html', html);
   gulp.watch('src/*.html').on('change', browserSync.reload);
-  gulp.watch('src/sass/**/*.scss', styles);
-  gulp.watch('src/img/*', images);
+  gulp.watch('src/sass/**/*.scss', styles, browserSync.reload);
   gulp.watch('src/scripts/*.js', js);
+  gulp.watch('src/img/*', images);
 }
 
+// Default task
 exports.default = gulp.series(styles, html, images, js, watch);
